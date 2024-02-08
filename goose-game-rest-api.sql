19df237a2683f591757d5f36c5b6831f8e1e5fbe
Nadia Roman <nad.roman14@gmail.com>
2024-02-08 12:26:22 +0200

    added .sql file

---
+++ goose-game-rest-api.sql
@@ -0,0 +1,21 @@
+create database gooseGameApi;
+Use gooseGameApi;
+
+insert into food
+value ("cookie", 10),
+("marshmallow", 5),
+("chocolate", 15);
+
+insert into activity
+value ("swiming", 5),
+("terrorizeing the neighbors", 15),
+("elegant honking", 10);
+
+insert into detergent
+value ("shampoo", 10),
+("water", 5),
+("soap", 15);
+
+insert into hat
+value (1, 0, 0, "no hat", 0),
+(2, 5, 5, "wizard hat", 5);
